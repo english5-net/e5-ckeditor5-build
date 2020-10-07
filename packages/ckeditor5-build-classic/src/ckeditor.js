9d6b854e1b42ea5bb69e5e078bb0acb039afcb22
Tony Nguyen <tony.nguyen1799@gmail.com>
2020-10-08 01:15:45 +0700

    Add more plugins

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -4,19 +4,23 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
@@ -28,7 +32,10 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -36,15 +43,20 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
-	Autoformat,
+    Autoformat,
+    Alignment,
+    FontSize,
+    FontFamily,
+    FontColor,
+    FontBackgroundColor,
 	Bold,
-	Italic,
+    Italic,
+    Strikethrough,
+    Underline,
 	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
 	Image,
-	ImageCaption,
+    ImageResize,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
@@ -53,9 +65,12 @@ ClassicEditor.builtinPlugins = [
 	List,
 	MediaEmbed,
 	Paragraph,
-	PasteFromOffice,
+    PasteFromOffice,
+    SimpleUploadAdapter,
 	Table,
-	TableToolbar,
+    TableToolbar,
+    TableProperties,
+    TableCellProperties,
 	TextTransformation
 ];
 
@@ -66,35 +81,65 @@ ClassicEditor.defaultConfig = {
 			'heading',
 			'|',
 			'bold',
-			'italic',
+            'italic',
+            'underline',
+            'strikethrough',
 			'link',
 			'bulletedList',
 			'numberedList',
 			'|',
 			'indent',
 			'outdent',
-			'|',
+            '|',
+            'fontFamily',
+            'fontSize',
+            'fontColor', 
+            'fontBackgroundColor',
+            '|', 'alignment:left', 'alignment:right', 'alignment:center', 'alignment:justify',
+            '|',
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
 			'undo',
 			'redo'
-		]
-	},
+        ],
+        shouldNotGroupWhenFull: true
+    },
+    fontSize: {
+        options: [
+            9,
+            11,
+            13,
+            'default',
+            17,
+            19,
+            21,
+            23,
+            25
+        ],
+        supportAllValues: true
+    },
 	image: {
+        styles: [
+            'full', 'alignLeft', 'alignCenter', 'alignRight'
+        ],
 		toolbar: [
 			'imageStyle:full',
-			'imageStyle:side',
+            'imageStyle:alignLeft',
+            'imageStyle:alignCenter',
+            'imageStyle:alignRight',
 			'|',
 			'imageTextAlternative'
 		]
-	},
+    },
 	table: {
 		contentToolbar: [
 			'tableColumn',
 			'tableRow',
-			'mergeTableCells'
+			'mergeTableCells',
+            'tableProperties',
+            'tableCellProperties'
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
